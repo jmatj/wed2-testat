c888a0bf9ea0d8eba72bc25ee86d6ad1c51f63cc
Jonas Matter <jonas.matter@hsr.ch>
2016-10-21 15:35:29 +0200

    fix date issue

--- controller/noteController.js
+++ controller/noteController.js
@@ -20,12 +20,13 @@ module.exports.newNote = function(req, res) {
             title: '',
             description: '',
             importance: 1,
-            dueDate: moment().format('YYYY-MM-DD')
+            duedate: moment().format('YYYY-MM-DD')
         });
 };
 
 module.exports.getNote = function(req, res) {
     noteService.get(req.params.id, function(err, note) {
+        note.duedate = moment(note.duedate).format('YYYY-MM-DD');
         res.render('createEditNote', note);
     });
 };
@@ -34,12 +35,14 @@ module.exports.createNote = function(req, res) {
     var note = req.body;
     note.createDate = Date.now();
     note.finishDate = null;
+    req.body.duedate = moment(req.body.duedate).format('YYYY-MM-DD H:m');
     noteService.add(req.body, function(err, note) {
         res.redirect('/');
     });
 };
 
 module.exports.updateNote = function(req, res) {
+    req.body.duedate = moment(req.body.duedate).format('YYYY-MM-DD H:m');
     noteService.update(req.params.id, req.body, function(err, note) {
         res.redirect('/');
     });
