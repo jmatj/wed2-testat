f3e6add3d98d140f062f674c28f668ffb0624b07
Jonas Matter <jonas.matter@hsr.ch>
2016-10-25 20:26:37 +0200

    fix #4

--- controller/configController.js
+++ controller/configController.js
@@ -5,20 +5,27 @@ module.exports.updateConfigurations = function(req, res, query, callback) {
         var sort = !!query.sort ? query.sort : 'duedate';
         var sortOrder = 1;
         var style = !!query.style ? query.style : 'light';
+        var hasSwappingChanges = false;
 
         //swap sort properties if query param is set and corresponds with the config value
         if (!!config['sort'] && !!query.sort && config['sort'].value === query.sort) {
             sortOrder = config['sort'].params.sortOrder == 1 ? -1 : 1;
+            hasSwappingChanges = true;
         }
         if (!!config['style'] && !!query.style && config['style'].value === query.style) {
             style = config['style'].value == 'light' ? 'dark' : 'light';
+            hasSwappingChanges = true;
         }
-        //update config
-        configService.set('sort', sort, {sortOrder: sortOrder}, function() {
-            configService.set('style', style, {}, function() {
-                loadConfigurations(req, res, callback);
+        //update config only on updates
+        if (hasConfigurationsChanges(hasSwappingChanges, config, query)) {
+            configService.set('sort', sort, {sortOrder: sortOrder}, function() {
+                configService.set('style', style, {}, function() {
+                    loadConfigurations(req, res, callback);
+                });
             });
-        });
+        } else {
+            callback(req, res, config);
+        }
     });
 };
 
@@ -26,4 +33,8 @@ function loadConfigurations(req, res, callback) {
     configService.get(['sort', 'style'], function(err, config) {
         callback(req, res, config);
     });
+}
+
+function hasConfigurationsChanges(hasSwappingChanges, config, query) {
+    return hasSwappingChanges || (!!query.sort && query.sort !== config['sort']) || config['sort'] === undefined || config['style'] === undefined;
 }
